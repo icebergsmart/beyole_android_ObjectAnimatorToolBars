a587584204026dfed87e8ea0275e62c55186d67d
xuejiawei <xuejiawei123@yeah.net>
2015-10-20 14:18:00 +0800

    Iceberg-X

--- src/com/beyole/objectanimatortest/MainActivity.java
+++ src/com/beyole/objectanimatortest/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,7 +44,11 @@ public class MainActivity extends Activity {
 
 	private void closeAnim() {
 		for (int i = 1; i < res.length; i++) {
-			ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i), "translationY", i * 60, 0);
+			float angle = (90 * 1.0f / (res.length - 1)) * (i-1);
+			PropertyValuesHolder holder1 = PropertyValuesHolder.ofFloat("translationX",  (float) (Math.sin((angle / 90) * 1.57) * 200),0);
+			PropertyValuesHolder holder2 = PropertyValuesHolder.ofFloat("translationY", (float) (Math.cos((angle / 90) * 1.57) * 200),0);
+			ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(imageViewList.get(i), holder1, holder2);
+			//ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i), "translationY", i * 60, 0);
 			animator.setDuration(300);
 			animator.start();
 			isNotExpand = true;
@@ -53,9 +58,15 @@ public class MainActivity extends Activity {
 
 	private void startAnim() {
 		for (int i = 1; i < res.length; i++) {
-			ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i), "translationY", 0, i * 60);
-			animator.setDuration(500);
-			animator.setFrameDelay(500);
+			float angle = (90 * 1.0f / (res.length - 1)) * (i-1);
+			PropertyValuesHolder holder1 = PropertyValuesHolder.ofFloat("translationX", 0, (float) (Math.sin((angle / 90) * 1.57) * 200));
+			PropertyValuesHolder holder2 = PropertyValuesHolder.ofFloat("translationY", 0, (float) (Math.cos((angle / 90) * 1.57) * 200));
+			ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(imageViewList.get(i), holder1, holder2);
+			// ObjectAnimator animator =
+			// ObjectAnimator.ofFloat(imageViewList.get(i), "translationY", 0, i *
+			// 60);
+			animator.setDuration(1000);
+			animator.setFrameDelay(500 * i);
 			animator.setInterpolator(new BounceInterpolator());
 			animator.start();
 			isNotExpand = false;
